3da1a0f512e945fced10805cdfad6ee275bc6e5d
Xeei <109275765+Xeei@users.noreply.github.com>
2026-04-06 16:31:57 +0700

    optimize build file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/munyin-dashboard/'
+  base: '/munyin-dashboard/',
+    build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          recharts: ['recharts'],
+          react:    ['react', 'react-dom'],
+        }
+      }
+    }
+  }
 })
